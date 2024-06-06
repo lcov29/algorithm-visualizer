23a2891de86f60fad6790ee7ec93326d189f8f65
lcov29 <76426289+lcov29@users.noreply.github.com>
2024-06-06 22:40:31 +0000

    Deploying to gh-pages from @ lcov29/algorithm-visualizer@d1a9e6a3908e069e4da5174f6f13e6d406802733 🚀

---
+++ app.bundle.js
@@ -0,0 +1,45 @@
+"use strict";
+(self["webpackChunkalgorithm_visualizer"] = self["webpackChunkalgorithm_visualizer"] || []).push([[524],{
+
+/***/ 35:
+/***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+
+;// CONCATENATED MODULE: ./packages/app/src/assets/icons/app-logo.svg
+const app_logo_namespaceObject = __webpack_require__.p + "4ff2a58328a53f82fff4.svg";
+// EXTERNAL MODULE: ./node_modules/react/index.js
+var react = __webpack_require__(540);
+;// CONCATENATED MODULE: ./packages/app/src/app.tsx
+
+
+
+function App() {
+    return (react.createElement("main", null,
+        react.createElement("h1", null, "Algorithm Visualizer"),
+        react.createElement("img", { src: app_logo_namespaceObject, width: "100px", height: "100px", alt: "Algorithm Visualizer" }),
+        react.createElement("p", null, "Coming soon")));
+}
+
+// EXTERNAL MODULE: ./node_modules/react-dom/client.js
+var client = __webpack_require__(338);
+;// CONCATENATED MODULE: ./packages/app/src/index.tsx
+
+
+
+const applicationContainer = document.getElementById('react-app');
+if (applicationContainer) {
+    const root = (0,client/* createRoot */.H)(applicationContainer);
+    root.render(react.createElement(react.StrictMode, null,
+        react.createElement(App, null)));
+}
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [96], () => (__webpack_exec__(35)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
