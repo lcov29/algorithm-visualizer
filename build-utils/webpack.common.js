cf4b10300386e8504330eeb7efd33fd84e9b58a2
lcov29 <lv_code@outlook.com>
2024-06-09 00:41:47 +0200

    build: update application entry in webpack config

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './packages/app/src/index.tsx',
+    app: './packages/app/index.tsx',
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
