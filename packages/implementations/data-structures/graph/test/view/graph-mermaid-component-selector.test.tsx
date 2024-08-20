e22b22a36dbffa446a1ba9dd99c36b43695338f9
lcov29 <lv_code@outlook.com>
2024-08-21 00:12:02 +0200

    test: add unit test for the GraphMermaidComponentSelector class

---
+++ packages/implementations/data-structures/graph/test/view/graph-mermaid-component-selector.test.tsx
@@ -0,0 +1,80 @@
+/**
+ * @jest-environment jsdom
+ */
+import { GraphMermaidComponentSelector } from '../../src/view/graph-mermaid-component-selector';
+import { MockMermaidSVGGraph } from './mocks/mock-mermaid-svg-graph';
+
+import { render, screen } from '@testing-library/react';
+import * as React from 'react';
+
+const selector = new GraphMermaidComponentSelector();
+
+function getNodeLabelText(nodeLabelElement: Element | null | undefined) {
+  return nodeLabelElement?.childNodes[0].textContent ?? '';
+}
+
+describe('GraphMermaidComponentSelector', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    render(<MockMermaidSVGGraph />);
+    const mockGraphRef = {
+      current: screen.getByTestId('mock-mermaid-svg-graph') as HTMLDivElement,
+    };
+    selector.setGraphReference(mockGraphRef);
+  });
+
+  describe('getNode()', () => {
+    it.each([
+      ['first', 0, 'A'],
+      ['second', 1, 'B'],
+      ['third', 2, 'C'],
+    ])('selects the %s graph node', (_, nodeId, expectedNodeLabelText) => {
+      const node = selector.getNode(nodeId);
+      const nodeLabelElements = node?.getElementsByClassName('nodeLabel');
+      expect(nodeLabelElements).toHaveLength(1);
+      expect(getNodeLabelText(nodeLabelElements?.[0])).toBe(
+        expectedNodeLabelText,
+      );
+    });
+  });
+
+  describe('getLabelOfNode()', () => {
+    it.each([
+      ['first', 0, 'A'],
+      ['second', 1, 'B'],
+      ['third', 2, 'C'],
+    ])('selects the %s node label', (_, nodeId, expectedNodeLabelText) => {
+      const nodeLabelElement = selector.getLabelOfNode(nodeId);
+      expect(getNodeLabelText(nodeLabelElement)).toBe(expectedNodeLabelText);
+    });
+  });
+
+  describe('getEdgeBetween()', () => {
+    it.each([
+      ['first', 0, 1],
+      ['second', 1, 2],
+      ['third', 0, 2],
+    ])(
+      'selects the %s edge between the nodes %s and %s',
+      (_, startNodeId, endNodeId) => {
+        const edge = selector.getEdgeBetween({ startNodeId, endNodeId });
+        expect(edge?.id).toContain(`L-${startNodeId}-${endNodeId}`);
+      },
+    );
+  });
+
+  describe('getLabelOfEdge()', () => {
+    it.each([
+      ['first', 0, 0, 1, '10'],
+      ['second', 1, 1, 2, '7'],
+      ['third', 2, 0, 2, '5'],
+    ])(
+      'selects the label of the %s edge between the nodes %s and %s',
+      (_, edgeId, __, ___, expectedEdgeLabelText) => {
+        const edge = selector.getLabelOfEdge(edgeId);
+        const edgeLabelText = edge?.childNodes[0].textContent ?? '';
+        expect(edgeLabelText).toBe(expectedEdgeLabelText);
+      },
+    );
+  });
+});
---
+++ packages/implementations/data-structures/graph/test/view/mocks/mock-mermaid-svg-graph.tsx
@@ -0,0 +1,235 @@
+import * as React from 'react';
+
+export function MockMermaidSVGGraph(): JSX.Element {
+  return (
+    <div data-testid="mock-mermaid-svg-graph">
+      <svg
+        aria-roledescription="flowchart-v2"
+        role="graphics-document document"
+        viewBox="-8 -8 219.76666259765625 95.16665649414062"
+        xmlns="http://www.w3.org/2000/svg"
+        width="100%"
+        id="graph">
+        <g>
+          <marker
+            orient="auto"
+            markerHeight="12"
+            markerWidth="12"
+            markerUnits="userSpaceOnUse"
+            refY="5"
+            refX="6"
+            viewBox="0 0 10 10"
+            className="marker flowchart"
+            id="graph_flowchart-pointEnd">
+            <path className="arrowMarkerPath" d="M 0 0 L 10 5 L 0 10 z"></path>
+          </marker>
+          <marker
+            orient="auto"
+            markerHeight="12"
+            markerWidth="12"
+            markerUnits="userSpaceOnUse"
+            refY="5"
+            refX="4.5"
+            viewBox="0 0 10 10"
+            className="marker flowchart"
+            id="graph_flowchart-pointStart">
+            <path className="arrowMarkerPath" d="M 0 5 L 10 10 L 10 0 z"></path>
+          </marker>
+          <marker
+            orient="auto"
+            markerHeight="11"
+            markerWidth="11"
+            markerUnits="userSpaceOnUse"
+            refY="5"
+            refX="11"
+            viewBox="0 0 10 10"
+            className="marker flowchart"
+            id="graph_flowchart-circleEnd">
+            <circle className="arrowMarkerPath" r="5" cy="5" cx="5"></circle>
+          </marker>
+          <marker
+            orient="auto"
+            markerHeight="11"
+            markerWidth="11"
+            markerUnits="userSpaceOnUse"
+            refY="5"
+            refX="-1"
+            viewBox="0 0 10 10"
+            className="marker flowchart"
+            id="graph_flowchart-circleStart">
+            <circle className="arrowMarkerPath" r="5" cy="5" cx="5"></circle>
+          </marker>
+          <marker
+            orient="auto"
+            markerHeight="11"
+            markerWidth="11"
+            markerUnits="userSpaceOnUse"
+            refY="5.2"
+            refX="12"
+            viewBox="0 0 11 11"
+            className="marker cross flowchart"
+            id="graph_flowchart-crossEnd">
+            <path
+              className="arrowMarkerPath"
+              d="M 1,1 l 9,9 M 10,1 l -9,9"></path>
+          </marker>
+          <marker
+            orient="auto"
+            markerHeight="11"
+            markerWidth="11"
+            markerUnits="userSpaceOnUse"
+            refY="5.2"
+            refX="-1"
+            viewBox="0 0 11 11"
+            className="marker cross flowchart"
+            id="graph_flowchart-crossStart">
+            <path
+              className="arrowMarkerPath"
+              d="M 1,1 l 9,9 M 10,1 l -9,9"></path>
+          </marker>
+          <g className="root">
+            <g className="clusters"></g>
+            <g className="edgePaths">
+              <path
+                className="edge-thickness-normal edge-pattern-solid flowchart-link LS-0 LE-1"
+                id="L-0-1-0"
+                d="M23.588,34.428C35.642,23.456,47.696,12.483,59.75,12.483C71.189,12.483,82.628,12.483,94.067,12.483"></path>
+              <path
+                className="edge-thickness-normal edge-pattern-solid flowchart-link LS-1 LE-2"
+                id="L-1-2-0"
+                d="M119.033,12.483C128.919,12.483,138.806,12.483,148.692,12.483C159.301,12.483,169.91,23.204,180.519,33.925"></path>
+              <path
+                className="edge-thickness-normal edge-pattern-solid flowchart-link LS-0 LE-2"
+                id="L-0-2-0"
+                d="M23.588,47.622C35.642,58.594,47.696,69.567,59.75,69.567C75.35,69.567,90.95,69.567,106.55,69.567C120.597,69.567,134.644,69.567,148.692,69.567C159.301,69.567,169.91,58.846,180.519,48.125"></path>
+            </g>
+            <g className="edgeLabels">
+              <g
+                transform="translate(59.75, 12.483329772949219)"
+                className="edgeLabel">
+                <g
+                  transform="translate(-9.316665649414062, -9.599998474121094)"
+                  className="label">
+                  <foreignObject
+                    height="19.199996948242188"
+                    width="18.633331298828125">
+                    <div>
+                      <span className="edgeLabel">10</span>
+                    </div>
+                  </foreignObject>
+                </g>
+              </g>
+              <g
+                transform="translate(148.69165802001953, 12.483329772949219)"
+                className="edgeLabel">
+                <g
+                  transform="translate(-4.658332824707031, -9.599998474121094)"
+                  className="label">
+                  <foreignObject
+                    height="19.199996948242188"
+                    width="9.316665649414062">
+                    <div>
+                      <span className="edgeLabel">7</span>
+                    </div>
+                  </foreignObject>
+                </g>
+              </g>
+              <g
+                transform="translate(106.54999542236328, 69.56665802001953)"
+                className="edgeLabel">
+                <g
+                  transform="translate(-4.658332824707031, -9.599998474121094)"
+                  className="label">
+                  <foreignObject
+                    height="19.199996948242188"
+                    width="9.316665649414062">
+                    <div>
+                      <span className="edgeLabel">5</span>
+                    </div>
+                  </foreignObject>
+                </g>
+              </g>
+            </g>
+            <g className="nodes">
+              <g
+                transform="translate(12.716667175292969, 41.024993896484375)"
+                data-id="0"
+                data-node="true"
+                id="flowchart-0-9"
+                className="node default default flowchart-label">
+                <circle
+                  height="34.19999694824219"
+                  width="25.433334350585938"
+                  r="12.716667175292969"
+                  ry="0"
+                  rx="0"></circle>
+                <g
+                  transform="translate(-5.216667175292969, -9.599998474121094)"
+                  className="label">
+                  <rect></rect>
+                  <foreignObject
+                    height="19.199996948242188"
+                    width="10.433334350585938">
+                    <div>
+                      <span className="nodeLabel">A</span>
+                    </div>
+                  </foreignObject>
+                </g>
+              </g>
+              <g
+                transform="translate(106.54999542236328, 12.483329772949219)"
+                data-id="1"
+                data-node="true"
+                id="flowchart-1-10"
+                className="node default default flowchart-label">
+                <circle
+                  height="34.19999694824219"
+                  width="24.966659545898438"
+                  r="12.483329772949219"
+                  ry="0"
+                  rx="0"></circle>
+                <g
+                  transform="translate(-4.983329772949219, -9.599998474121094)"
+                  className="label">
+                  <rect></rect>
+                  <foreignObject
+                    height="19.199996948242188"
+                    width="9.966659545898438">
+                    <div>
+                      <span className="nodeLabel">B</span>
+                    </div>
+                  </foreignObject>
+                </g>
+              </g>
+              <g
+                transform="translate(191.0583267211914, 41.024993896484375)"
+                data-id="2"
+                data-node="true"
+                id="flowchart-2-11"
+                className="node default default flowchart-label">
+                <circle
+                  height="34.19999694824219"
+                  width="25.416671752929688"
+                  r="12.708335876464844"
+                  ry="0"
+                  rx="0"></circle>
+                <g
+                  transform="translate(-5.208335876464844, -9.599998474121094)"
+                  className="label">
+                  <rect></rect>
+                  <foreignObject
+                    height="19.199996948242188"
+                    width="10.416671752929688">
+                    <div>
+                      <span className="nodeLabel">C</span>
+                    </div>
+                  </foreignObject>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+}
