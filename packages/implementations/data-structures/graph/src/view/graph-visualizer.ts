aff806729c3ff02f5078ebf80fecd45f73c1ce37
lcov29 <lv_code@outlook.com>
2024-08-20 15:11:09 +0200

    feat: add handler for node highlighted event to the graph visualizer

--- packages/implementations/data-structures/graph/src/view/graph-visualizer.ts
+++ packages/implementations/data-structures/graph/src/view/graph-visualizer.ts
@@ -1,6 +1,7 @@
 import { IEventHandlerChain } from '@algorithm-visualizer/event-handling-contract';
 import {
   GraphVisualizationEvent,
+  IGraphComponentSelector,
   IGraphSVGRenderEngine,
   IGraphVisualizer,
 } from '@algorithm-visualizer/graph-contract';
@@ -10,18 +11,19 @@ import React from 'react';
 interface GraphVisualizerArgs {
   eventHandlerChain: IEventHandlerChain<GraphVisualizationEvent>;
   graphSVGRenderEngine: IGraphSVGRenderEngine<string>;
+  graphComponentSelector: IGraphComponentSelector;
 }
 
 export class GraphVisualizer implements IGraphVisualizer {
   private _eventHandlerChain: IEventHandlerChain<GraphVisualizationEvent>;
   private _graphSVGRenderEngine: IGraphSVGRenderEngine<string>;
-  private _graphRef: React.RefObject<HTMLDivElement>;
+  private _graphComponentSelector: IGraphComponentSelector;
   private _setGraphSVGString: React.Dispatch<React.SetStateAction<string>>;
 
   constructor(args: GraphVisualizerArgs) {
     this._eventHandlerChain = args.eventHandlerChain;
     this._graphSVGRenderEngine = args.graphSVGRenderEngine;
-    this._graphRef = { current: null };
+    this._graphComponentSelector = args.graphComponentSelector;
     this._setGraphSVGString = () => {};
     this._initializeEventHandlerChain();
   }
@@ -30,7 +32,7 @@ export class GraphVisualizer implements IGraphVisualizer {
     graphRef: React.RefObject<HTMLDivElement>,
     setGraphSVGString: React.Dispatch<React.SetStateAction<string>>,
   ) {
-    this._graphRef = graphRef;
+    this._graphComponentSelector.setGraphReference(graphRef);
     this._setGraphSVGString = setGraphSVGString;
   }
 
@@ -39,7 +41,9 @@ export class GraphVisualizer implements IGraphVisualizer {
   }
 
   private _initializeEventHandlerChain() {
-    this._eventHandlerChain.add(event => this._handleGraphCreatedEvent(event));
+    this._eventHandlerChain
+      .add(event => this._handleGraphCreatedEvent(event))
+      .add(event => this._handleNodeHighlightedEvent(event));
   }
 
   private async _handleGraphCreatedEvent(event: GraphVisualizationEvent) {
@@ -48,6 +52,15 @@ export class GraphVisualizer implements IGraphVisualizer {
     }
     const svg = await this._graphSVGRenderEngine.render(event);
     this._setGraphSVGString(svg);
+    return true;
+  }
+
+  private async _handleNodeHighlightedEvent(event: GraphVisualizationEvent) {
+    if (event.name !== 'node-highlighted') {
+      return false;
+    }
+    const node = this._graphComponentSelector.getNode(event.nodeId);
+    node?.classList.add('nodeHighlighted');
     return true;
   }
 }
