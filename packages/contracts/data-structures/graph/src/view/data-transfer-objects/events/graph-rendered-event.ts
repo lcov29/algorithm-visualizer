b6cee0dca8b9da9278a5dda86a8c0621ebd3217a
lcov29 <lv_code@outlook.com>
2024-08-26 11:10:02 +0200

    refactor: add property render direction to the graph rendered event

--- packages/contracts/data-structures/graph/src/view/data-transfer-objects/events/graph-rendered-event.ts
+++ packages/contracts/data-structures/graph/src/view/data-transfer-objects/events/graph-rendered-event.ts
@@ -3,20 +3,24 @@ import { BaseEvent } from '@algorithm-visualizer/event-handling-contract';
 
 import { IEdge } from '../../../structure';
 import { INode } from '../../interfaces/graph-node';
+import { GraphRenderDirection } from '../../types';
 
 interface IGraphRenderedEventArgs {
   nodes: INode[];
   edges: IEdge[];
+  renderDirection: GraphRenderDirection;
 }
 
 export class GraphRenderedEvent extends BaseEvent<'graph-rendered'> {
   private _nodes: INode[];
   private _edges: IEdge[];
+  private _renderDirection: GraphRenderDirection;
 
   constructor(args: IGraphRenderedEventArgs) {
     super('graph-rendered');
     this._nodes = args.nodes;
     this._edges = args.edges;
+    this._renderDirection = args.renderDirection;
   }
 
   get nodes() {
@@ -25,6 +29,10 @@ export class GraphRenderedEvent extends BaseEvent<'graph-rendered'> {
 
   get edges() {
     return this._edges;
+  }
+
+  get renderDirection() {
+    return this._renderDirection;
   }
 
   set nodes(input: INode[]) {
@@ -36,6 +44,12 @@ export class GraphRenderedEvent extends BaseEvent<'graph-rendered'> {
   set edges(input: IEdge[]) {
     throw new InvalidOperationError({
       message: 'Writing to readonly property edges is forbidden',
+    });
+  }
+
+  set renderDirection(direction: GraphRenderDirection) {
+    throw new InvalidOperationError({
+      message: 'Writing to readonly property renderDirection is forbidden',
     });
   }
 }
---
+++ packages/contracts/data-structures/graph/test/view/data-transfer-objects/events/graph-rendered-event.test.ts
@@ -0,0 +1,53 @@
+import { InvalidOperationError } from '@algorithm-visualizer/error-handling-contract';
+
+import {
+  GraphRenderDirection,
+  GraphRenderedEvent,
+  IEdge,
+  INode,
+} from '../../../../src';
+
+describe('GraphRenderedEvent', () => {
+  const mockNodes: INode[] = [
+    { id: 0, label: 'A' },
+    { id: 1, label: 'B' },
+    { id: 2, label: 'C' },
+  ];
+  const mockEdges: IEdge[] = [
+    { id: 0, startNodeId: 0, endNodeId: 1 },
+    { id: 1, startNodeId: 1, endNodeId: 2 },
+  ];
+  const mockRenderDirection: GraphRenderDirection = 'Left-To-Right';
+
+  const graphRenderedEvent = new GraphRenderedEvent({
+    nodes: mockNodes,
+    edges: mockEdges,
+    renderDirection: mockRenderDirection,
+  });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe.each([
+    ['nodes', mockNodes],
+    ['edges', mockEdges],
+    ['renderDirection', mockRenderDirection],
+  ])('%s()', (methodName, expectedResult) => {
+    it(`getter returns the specified ${methodName} value`, () => {
+      // @ts-expect-error invoke method by string name
+      expect(graphRenderedEvent[methodName]).toBe(expectedResult);
+    });
+
+    it('setter throws an invalid operation error', () => {
+      expect(() => {
+        // @ts-expect-error invoke method by string name
+        graphRenderedEvent[methodName] = expectedResult;
+      }).toThrow(
+        new InvalidOperationError({
+          message: `Writing to readonly property ${methodName} is forbidden`,
+        }),
+      );
+    });
+  });
+});
